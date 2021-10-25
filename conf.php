7155f4809234fea7c5634326964843fd56e032ac
Irina Merkulova <irina.merkulova@tthk.ee>
2021-10-25 10:11:16 +0300

    25.okt tunnitöö

---
+++ conf.php
@@ -0,0 +1,9 @@
+<?php
+$kasutaja="irinamerkulova20";
+$andmebaas="irinamerkulova20";
+$parool="123456";
+$serverinimi="localhost";
+$yhendus=new mysqli($serverinimi, $kasutaja,
+    $parool, $andmebaas);
+$yhendus->set_charset("utf8");
+?>
